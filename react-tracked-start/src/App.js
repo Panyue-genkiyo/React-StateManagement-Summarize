235dd10a04619cf9a378c60c3db5e2b017e9c9eb
panyue <90938238g@gmail.com>
2021-11-20 10:43:13 +0800

    结合immer和react-tracked(context)->Todolist

--- react-tracked-start/src/App.js
+++ react-tracked-start/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import {SharedProvider} from "./store";
 import {PersonProvider} from './store/person';
+// import { TodoProviderReducer } from "./store/tr";
+import { TodoStateImmerProvider } from './store/tui'
 import Count from "./components/Count";
 import Text from "./components/Text";
 import EditPerson from "./components/EditPerson";
 import ShowPerson from "./components/ShowPerson";
+// import TodoList from "./components/TodoList_Reducer_Version/TodoList";
+import TodoList from './components/TodoList_UseStateImmer_Version/TodoList'
 
 function App() {
 
@@ -20,6 +24,13 @@ function App() {
               <EditPerson/>
               <ShowPerson/>
           </PersonProvider>
+          <hr/>
+          {/*<TodoProviderReducer>*/}
+          {/*    <TodoList/>*/}
+          {/*</TodoProviderReducer>*/}
+          <TodoStateImmerProvider>
+              <TodoList/>
+          </TodoStateImmerProvider>
       </>
   )
 }
